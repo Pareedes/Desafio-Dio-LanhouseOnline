9a95a46094becd0e6adfb150e601c08e964e51b9
Gabriel Paredes <gparedesferreira@gmail.com>
2023-04-25 21:51:44 -0300

    Add files via upload

---
+++ LanhouseDigital/src/main/java/me/dio/academia/digital/entity/form/ComputadorForm.java
@@ -0,0 +1,12 @@
+package me.dio.academia.digital.entity.form;
+
+import me.dio.academia.digital.entity.Cliente;
+
+public class ComputadorForm {
+
+  private Long idComputador;
+
+  private String marca;
+
+  private Long idCliente;
+}
