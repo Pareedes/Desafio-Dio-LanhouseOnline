e60998c6ad127fa99d21521e735822c6207df840
Gabriel Paredes <gparedesferreira@gmail.com>
2023-04-28 21:28:05 -0300

    Update LanhouseDigitalApplication.java

--- LanhouseDigital/src/main/java/me/dio/academia/digital/LanhouseDigitalApplication.java
+++ LanhouseDigital/src/main/java/me/dio/academia/digital/LanhouseDigitalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcademiaDigitalApplication {
+public class LanhouseDigitalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AcademiaDigitalApplication.class, args);
+		SpringApplication.run(LanhouseDigitalApplication.class, args);
 	}
 
 }
